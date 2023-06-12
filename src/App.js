87e4378f936103e2c28bc978a5ff792075e37759
shokier1 <129298265+shokier1@users.noreply.github.com>
2023-06-12 07:23:36 +0300

    upload

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import Header from './components/Header'
 import Home from './components/Home'
 import Footer from './components/Footer'
 import Details from './components/Details'
-import { Route, Routes } from 'react-router-dom'
+import { createHashRouter, Route, Routes } from 'react-router-dom'
 
 import axios from 'axios'
+
 
 function App() {
   
   const [movies , setMovies] = useState ([])
-
+  
   const getAllMovies = async ()=>{
     const res = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=52ef927bbeb21980cd91386a29403c78&language=ar`)
     setMovies (res.data.results); 
@@ -20,20 +21,20 @@ function App() {
       const res = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=52ef927bbeb21980cd91386a29403c78&query=${word}&language=ar`)
       setMovies (res.data.results) ;
   }//search
- 
+
   useEffect( ()=>{
     getAllMovies()
   } , [])  
    
   return (
     <>
-   <Header search={search} />
-   <Routes>
-  <Route path='/' element={<Home movies={movies} />}/>
-  <Route path='/details/:id' element={<Details/>}/>
-   </Routes>
-    
-   <Footer/>
+    <Header search={search} />
+    <Routes>
+   <Route path='/' element={<Home movies={movies} />}/>
+   <Route path='/details/:id' element={<Details/>}/>
+    </Routes>
+
+    <Footer/>
     </>
   )
   }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import {createHashRouter, BrowserRouter ,RouterProvider } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.rtl.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle'
 import './style.css';
 import App from './App';
-
+const router = createHashRouter([])
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+
+ 
   <React.StrictMode>
     <BrowserRouter>
      <App/>
+       <RouterProvider router={router} />
     </BrowserRouter>
-   
+  
   </React.StrictMode>
 );
 
